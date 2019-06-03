c913456158723324cf4fd363eb7e87f336bd61b3
huyong007 <1341696804@qq.com>
2019-06-03 08:46:27 +0800

    change something

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 /* eslint-env browser */
 import './assets/css/style.css';
-
+const mytitle = document.createElement('div');
+mytitle.textContent = 'this is my first poi app.'
+mytitle.className = 'mytitle';
 const title = document.createElement('h1');
+
 title.textContent = 'Hello Poi!';
 title.className = 'title';
 
@@ -12,6 +15,7 @@ tip.className = 'tip';
 const app = document.getElementById('app');
 
 if (app) {
+  app.appendChild(mytitle);
   app.appendChild(title);
   app.appendChild(tip);
 }
