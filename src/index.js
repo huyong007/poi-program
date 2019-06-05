19d2688f392bf06fd6d9e0dbe8ecb0c78d752dc9
huyong007 <1341696804@qq.com>
2019-06-05 09:07:26 +0800

    react

--- src/index.js
+++ src/index.js
@@ -1,21 +1,63 @@
-/* eslint-env browser */
-import './assets/css/style.css';
-const mytitle = document.createElement('div');
-mytitle.textContent = 'this is my first poi app.'
-mytitle.className = 'mytitle';
-const title = document.createElement('h1');
-
-title.textContent = 'Hello Poi!';
-title.className = 'title';
-
-const tip = document.createElement('div');
-tip.textContent = 'Edit src/index.js and save to reload.';
-tip.className = 'tip';
-
-const app = document.getElementById('app');
-
-if (app) {
-  app.appendChild(mytitle);
-  app.appendChild(title);
-  app.appendChild(tip);
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
 }
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
